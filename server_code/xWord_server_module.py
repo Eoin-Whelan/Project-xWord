3957b25f48cfa6de7ba79616ace0b2a11a2c1960
c00164354@itcarlow.ie <c00164354@itcarlow.ie>
2021-01-22 23:12:48 +0000

    Edited xWord_form.ItemTemplate1, xWord_form.pattern_results and xWord_server_module

--- server_code/xWord_server_module.py
+++ server_code/xWord_server_module.py
@@ -9,22 +9,26 @@ from anvil.google.drive import app_files
 import anvil.server
 
 @anvil.server.callable
-
-
 def import_dictionary():
   fStr = app_files.words_txt.get_bytes()
   fStr = str(fStr, "utf-8")
   fStr = fStr.split()
   fSet = {line.strip("'s").lower() for line in fStr}
   fSet = sorted(fSet)[1::]
-  return fSet
+  print(fSet[:30])
+  return fSet[:30]
   
 
 def find_possible_matches(pattern):
+  
     """Given any pattern of the type "__a___b__c", this function
        looks up and returns all the potential matches for the
        pattern in the Linux dictionary of words."""
-
+    fStr = app_files.words_txt.get_bytes()
+    fStr = str(fStr, "utf-8")
+    fStr = fStr.split()
+    fSet = {line.strip("'s").lower() for line in fStr}
+    fSet = sorted(fSet)[1::]
     def match_pattern(w, p):
         """Returns True if 'w' matches 'p', False otherwise."""
         letters = {k: v for k, v in enumerate(p) if v != "_"}
